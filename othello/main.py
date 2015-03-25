436c5bd339f652dd89a209aac950b5173a2f2d29
Satyen Akolkar <akolkar@ualberta.ca>
2015-03-25 12:08:17 -0600

    added othello folder for program files

---
+++ othello/main.py
@@ -0,0 +1,37 @@
+import os, sys
+import pygame
+from pygame.locals import *
+
+if not pygame.font: print("Warning, fonts disabled")
+if not pygame.mixer: print("Warning, sound disabled")
+
+class OthelloMain:
+    """
+    The Main Othello Class
+    """
+    
+    def __init__(self, width=640, height=640):
+        """
+        Initalize the game window
+        """
+        
+        # initialize pygame
+        pygame.init()
+        self.width = width
+        self.height = height
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        pygame.display.set_caption("Othello")
+        #pygame.mouse.set_visible(0)
+        
+    def MainLoop(self):
+        """
+        Main loop of the game
+        """
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+if __name__ == "src.main":
+    MainWindow = OthelloMain()
+    MainWindow.MainLoop()
