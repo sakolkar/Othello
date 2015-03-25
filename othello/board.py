c6683764dd2a99ce77ef0c48168370e3605902bc
Satyen Akolkar <akolkar@ualberta.ca>
2015-03-25 12:40:06 -0600

    added board file

---
+++ othello/board.py
@@ -0,0 +1,16 @@
+
+# board datastructure that holds all the tiles
+class Board():
+    
+    # initalization
+    def __init__(self):
+        
+        # dictionary of 64 tiles
+        # count of black peices
+        # count of white peices
+        # list of playable tiles for black
+        # list of playable tiles for white
+        
+        pass
+        
+    # 
